ce4393205d56780326f041edd48ca4de3f878537
Juniorgirouxdev <142844490+Juniorgirouxdev@users.noreply.github.com>
2023-11-13 09:04:47 -0300

    add animaçõs integrada a api da ScrollReveal

---
+++ reveal.js
@@ -0,0 +1,79 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal(".no-ritmo-header", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal('.box-texto-main',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+});
+
+revelar.reveal('.img01',
+{
+  duration: 2000,
+  distance: '90px',
+  origin:'left'
+});
+
+revelar.reveal('.img03',
+{
+  duration: 2000,
+  distance: '90px',
+  origin:'left',
+  delay: '800'
+});
+
+revelar.reveal('.img02',
+{
+  duration: 2000,
+  distance: '90px',
+  origin:'rigth'
+});
+
+revelar.reveal('.img04',
+{
+  duration: 2000,
+  distance: '90px',
+  origin:'rigth',
+  delay: '800'
+});
+
+revelar.reveal('.button-mais',
+{
+  duration: 2000,
+  distance: '50px',
+  delay: 1500
+});
+
+
+revelar.reveal('.icon01',
+{
+  duration: 2000,
+  distance: '50px',
+});
+
+revelar.reveal('.icon02',
+{
+  duration: 2000,
+  distance: '50px',
+  delay:'500'
+});
+
+revelar.reveal('.icon03',
+{
+  duration: 2000,
+  distance: '50px',
+  delay:'600'
+});
+
+revelar.reveal('.icon04',
+{
+  duration: 2000,
+  distance: '50px',
+  delay:'700'
+});
+
